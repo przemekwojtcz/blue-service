9d42b5628dbb2efe335b1090c030e5a8acda9720
przemekwojtcz <przemekwojtcz@gmail.com>
2023-06-25 18:28:47 +0200

    WIP

--- src/main/java/com/pwoj/service_blue/test/StandardGreenServiceClient.java
+++ src/main/java/com/pwoj/service_blue/test/StandardGreenServiceClient.java
@@ -1,15 +1,22 @@
 package com.pwoj.service_blue.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class StandardGreenServiceClient implements GreenServiceClient{
+    Logger logger = LoggerFactory.getLogger(StandardGreenServiceClient.class);
+    @Value(value = "${greenService.baseUrl}")
+    private String baseUrl;
     @Override
     public GreenServiceResponse test() {
         RestTemplate restTemplate = new RestTemplate();
         String url
-                = "http://localhost:8081/test";
+                = baseUrl +"/test";
+        logger.info("Green service url: " + url);
         var response
                 = restTemplate.getForEntity(url, GreenServiceResponse.class);
         return response.getBody();
